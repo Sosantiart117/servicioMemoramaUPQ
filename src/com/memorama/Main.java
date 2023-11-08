830f54cf5a833af1543b85aa11e2e6d487037d9b
Santiago Orozco <devsos117@gmail.com>
2023-11-07 19:48:09 -0600

    Rework the file structure

---
+++ src/com/memorama/Main.java
@@ -0,0 +1,191 @@
+package com.memorama;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.File;
+
+public class Main implements ActionListener{
+
+	public static String PATH = "./";
+	public static String FILESDIR = "media/img/";
+	public static String SELECTEDDIR;
+	public static JFrame frame;
+
+	private static JLayeredPane base;
+	private static JButton startB;
+	private static LevelSelector lvlSelector ;
+	private static JComboBox<String> dirSelector ;
+	private static Color cBase = 		new Color(0x0C1E42);
+	private boolean isDirSelected = false;
+
+	public static String titleImagePath;
+	private static String HOMEPATH;
+
+	public static void main(String args[]){
+		HOMEPATH = args.length!=0?args[0]:"./";
+		new Main();
+		frame.setVisible(true);
+	}
+
+
+
+	// Pantalla principal
+	public Main (){
+		// Set correct directory
+		setDir();
+		// Inicia la ventana
+		setFrame();
+		// Inicia el layout para la ventana
+		initBase();
+		// Add title
+		initTitle();
+		// Dificultad
+		initLevel();
+		// Distintis temas
+		this.initDirSelector();
+		// Botton de inicio
+		this.initSButton();
+
+	}
+
+	private void setDir() {
+		PATH = "./";
+		FILESDIR = "media/img/";
+		PATH=HOMEPATH;
+		PATH.replace("/", File.separator);
+		FILESDIR = PATH + FILESDIR;
+		FILESDIR.replace("/", File.separator);
+		titleImagePath = PATH+"media/icons/title.png";
+		titleImagePath.replace("/",File.separator);
+	}
+
+	private static void setFrame(){
+		frame = new JFrame();
+		// técnico 
+		frame.setTitle("Memorama"); //Nombre de la ventana
+		frame.setResizable(false); // tamaño fijo
+		frame.setSize(500,600); // x y 
+		frame.setLocationRelativeTo(null); //aparece en el centro del moitor
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Termina cuando cierre
+		
+		// Apraciencia
+		// El image hay que importarlo y tener un archivo
+		frame.getContentPane().setBackground(cBase); //hacerle un color base
+		frame.setLayout(null);
+		
+	}
+
+	private static void initBase(){
+		// layered
+		base = new JLayeredPane();
+		base.setBackground(cBase);
+		frame.add(base);
+	}
+
+	private static void initTitle(){
+		// Subtitle 
+		JLabel AL = new JLabel("Algebra Lineal");
+		AL.setFont(new Font("Arial", Font.PLAIN,20));
+		AL.setForeground(Color.WHITE);
+		AL.setHorizontalAlignment(JLabel.CENTER);
+		AL.setBounds(100, 30, 300, 20);
+		frame.add(AL);
+		// Inicia el titulo principal
+		ImageIcon tIcon = new ImageIcon(titleImagePath);
+		// JLabel title = new JLabel("Algebra", JLabel.CENTER);
+		JLabel title = new JLabel();
+		title.setText("Memorama");
+		title.setHorizontalTextPosition(JLabel.CENTER);
+		title.setVerticalTextPosition(JLabel.TOP);
+		title.setFont(new Font("Arial",Font.BOLD,40));
+		title.setForeground(Color.WHITE);
+		title.setIconTextGap(50);
+		title.setIcon(tIcon);
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setBounds(50, 50, 400, 300);
+		frame.add(title);
+	}
+
+	private void initDirSelector(){
+		// Selector de Directorios 
+		String[] dirs = getDirs(FILESDIR);
+	 	dirSelector = new JComboBox<String>(dirs);
+		dirSelector.setSelectedItem(null);
+		dirSelector.setBounds(190, 350,120,20);
+		dirSelector.addActionListener(this);
+		frame.add(dirSelector);
+	}
+
+	private static void initLevel(){
+		// Selector de dificultad
+	 	lvlSelector = new LevelSelector();
+		lvlSelector.setBounds(190, 380,120,20);
+		frame.add(lvlSelector);
+	}
+
+
+	public static String[] getDirs(String dirName){
+		String[] dirs = new File(dirName).list(); 
+		return dirs;
+	}
+
+	private void initSButton(){
+		// Buton de inicion
+		startB = new JButton("Inicio");
+		startB.setBounds(200, 420, 100, 50);
+		startB.addActionListener(this);
+		startB.setActionCommand("Start");
+		frame.add(startB);
+	}
+
+	private static void startMemorama(int lvl,String lvlDir){
+		// Inicia el Juego
+		new Memorama(lvl, lvlDir).setVisible(true);
+		frame.setVisible(false);
+		frame.dispose();
+	}
+
+	private static boolean askName(){
+		// POP up para pedir el nombre
+		Memorama.userName = JOptionPane.showInputDialog("¿Como te llamas?");
+		if(Memorama.userName == null) return false;
+		return true;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource() == dirSelector){
+			isDirSelected = true;
+			Memorama.subject = dirSelector.getSelectedItem().toString();
+			SELECTEDDIR = FILESDIR + Memorama.subject;
+			lvlSelector.updateSelection(SELECTEDDIR);
+		}
+
+		// If button selected
+		if(e.getSource() == startB){
+			if(! isDirSelected) return;
+			if(! askName()) return;
+			int selectedLevel = lvlSelector.getSelectedLevel() +1;
+
+			FILESDIR +=
+				dirSelector.getSelectedItem().toString() +
+				File.separator + 
+				lvlSelector.getSelectedLevel()+
+				File.separator;
+
+			startMemorama(selectedLevel,FILESDIR);// start game
+			frame.dispose();
+		}
+	}
+
+}
---
+++ src/com/memorama/OptionsPanel.java
@@ -0,0 +1,146 @@
+package com.memorama;
+import javax.swing.JToggleButton;
+import javax.swing.border.TitledBorder;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.plaf.DimensionUIResource;
+
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Font;
+
+
+public class OptionsPanel extends JPanel{
+
+	private static Color cBase = 		new Color(0x0C1E42);
+	private static Color cMain = 		new Color(0x0B378F);
+	// private static Color cOpt = 		new Color(0x401C15);
+	// private static Color cSec = 		new Color(0x2E4205);
+	// private static Color cContr = 	new Color(0x668F13);
+
+	public JLabel timeLabel;
+	public JLabel nameLabel;
+	public JLabel subjectLabel;
+	public LevelSelector levelSelector;
+	public JButton sButton;
+	public JButton resButton;
+	public JToggleButton pButton;
+	public JButton bButton;
+	
+	public static Font optFont;
+
+	OptionsPanel(int winSize,int level,String timeText,ActionListener listener){
+		int pannelWidth = winSize *5;
+		optFont = new Font("Hack",Font.BOLD,26);
+
+		// Panel al lado
+		this.setPreferredSize(
+				new DimensionUIResource(pannelWidth,100));
+		this.setLayout(null);
+		this.setBackground(cMain);
+
+		// Name Label
+		if(Memorama.userName == null || Memorama.userName.isBlank())
+			Memorama.userName = "Nombre";
+		this.nameLabel = new JLabel(Memorama.userName);
+		this.nameLabel.setFocusable(false);
+		this.nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		this.nameLabel.setVerticalAlignment(JLabel.CENTER);
+		this.nameLabel.setForeground(cMain);
+		this.nameLabel.setOpaque(true);
+		this.nameLabel.setBackground(cBase);
+		this.nameLabel.setFont(optFont);
+		this.nameLabel.setBounds((pannelWidth-200)/2,50,200,80);
+		this.nameLabel.setBorder(
+			BorderFactory.createTitledBorder(
+						BorderFactory.createLineBorder(cMain),
+						"Nombre:",
+						TitledBorder.LEFT,
+						TitledBorder.BELOW_TOP,
+						new Font("Hack",Font.BOLD,16),
+						cMain
+					));
+		this.add(nameLabel);
+
+		// TimeLabel
+		this.timeLabel = new JLabel(timeText);
+		this.timeLabel.setFocusable(false);
+		this.timeLabel.setHorizontalAlignment(JLabel.CENTER);
+		this.timeLabel.setVerticalAlignment(JLabel.CENTER);
+		this.timeLabel.setBackground(cBase);
+		this.timeLabel.setOpaque(true);
+		this.timeLabel.setForeground(cMain);
+		this.timeLabel.setFont(optFont);
+		this.timeLabel.setBounds((pannelWidth-200)/2,150,200,200);
+		this.add(timeLabel);
+		
+		// Subject Label
+		this.subjectLabel = new JLabel(Memorama.subject);
+		this.subjectLabel.setFocusable(false);
+		this.subjectLabel.setHorizontalAlignment(JLabel.CENTER);
+		this.subjectLabel.setVerticalAlignment(JLabel.CENTER);
+		this.subjectLabel.setForeground(cBase);
+		this.subjectLabel.setFont(optFont);
+		this.subjectLabel.setBounds((pannelWidth-250)/2, 365, 250, 80);
+		this.subjectLabel.setBorder(
+			BorderFactory.createTitledBorder(
+						BorderFactory.createLineBorder(cBase),
+						"Tema:",
+						TitledBorder.LEFT,
+						TitledBorder.BELOW_TOP,
+						new Font("Hack",Font.BOLD,16),
+						cBase
+					));
+		this.add(subjectLabel);
+
+		// Level Selector
+		this.levelSelector = new LevelSelector(Main.SELECTEDDIR);
+		this.levelSelector.setSelectedIndex(level-1);
+		this.levelSelector.setFocusable(false);
+		this.levelSelector.setBounds((pannelWidth-120)/2, 460, 120, 20);
+		this.levelSelector.setEnabled(false);
+		this.add(levelSelector);
+
+		// Start Button
+		this.sButton = new JButton("Inicio");
+		this.sButton.setActionCommand("start");
+		this.sButton.setFocusable(false);
+		this.sButton.setBounds((pannelWidth-200)/2, 580, 200, 50);
+		this.sButton.setFont(optFont);
+		this.sButton.addActionListener(listener);
+		this.add(sButton);
+
+		// Reset Button
+		this.resButton = new JButton("Reinicio");
+		this.resButton.setActionCommand("restart");
+		this.resButton.setFocusable(false);
+		this.resButton.setBounds((pannelWidth-250)/2, 500, 250, 50);
+		this.resButton.setFont(optFont);
+		this.resButton.setEnabled(false);
+		this.resButton.addActionListener(listener);
+		this.add(resButton);
+
+		// Pause Button
+		this.pButton = new JToggleButton("Pausa");
+		this.pButton.setActionCommand("stop");
+		this.pButton.setFocusable(false);
+		this.pButton.setBounds((pannelWidth-200)/2, 580, 200, 50);
+		this.pButton.setFont(optFont);
+		this.pButton.setVisible(false);
+		this.pButton.addActionListener(listener);
+		this.add(pButton);
+
+		// Pause Button
+		this.bButton = new JButton("<");
+		this.bButton.setActionCommand("back");
+		this.bButton.setFocusable(false);
+		this.bButton.setBounds(25, 25, 50, 50);
+		this.bButton.setFont(optFont);
+		this.bButton.setVisible(true);
+		this.bButton.addActionListener(listener);
+		this.add(bButton);
+	}
+	
+}
